1666a0a01562ccf9f1b0d527dace1a8e3707f0b6
Aidan Joseph Houlihan <aidan.houlihan@yale.edu>
2023-05-29 10:17:53 -0400

    added options to allow default prefixes/values

--- options.js
+++ options.js
@@ -1,10 +1,13 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
-    const color = document.getElementById('color').value;
-    const likesColor = document.getElementById('catalog_number').checked;
+    const catalogPrefix = document.getElementById('catalog_prefix').value;
+    const lifeStage = document.getElementById('life_stage').value;
+    const individualCount = document.getElementById('individual_count').value;
+    const country = document.getElementById('country').value;
   
     chrome.storage.sync.set(
-      { favoriteColor: color, likesColor: likesColor },
+      { catalogPrefixG: catalogPrefix, lifeStageG: lifeStage,
+      individualCountG: individualCount, countryG: country },
       () => {
         // Update status to let user know options were saved.
         const status = document.getElementById('status');
@@ -20,10 +23,12 @@ const saveOptions = () => {
   // stored in chrome.storage.
   const restoreOptions = () => {
     chrome.storage.sync.get(
-      { favoriteColor: 'red', likesColor: true },
+      //{ favoriteColor: 'red', likesColor: true },
       (items) => {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('catalog_number').value = items.likesColor;
+        document.getElementById('catalog_prefix').value = items.catalogPrefixG;
+        document.getElementById('life_stage').value = items.lifeStageG;
+        document.getElementById('individual_count').value = items.individualCountG;
+        document.getElementById('country').value = items.countryG;
       }
     );
   };
